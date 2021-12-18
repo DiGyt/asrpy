b36f029c359dfb4481f5624854eb4e4a1a314503
dguetlin <dirk.guetlin@gmail.com>
2021-12-18 15:12:33 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "asrpy",
+    version = "0.0.1",
+
+    author = "Dirk Gütlin",
+    author_email = "dirk.guetlin@gmail.com",
+    description = ("Artifact Subspace Reconstruction in Python."),
+    license = "BSD-3",
+    keywords = ["Signal Processing", "EEG", "MEG", "Noise Removal",
+                "Noise Detection", "Automated Cleaning"],
+    url = "https://github.com/DiGyt/asrpy",
+    packages=['asr'],
+    include_package_data=True,
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "mne",
+    ],
+)
