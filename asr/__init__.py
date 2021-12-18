427109bd2c4f50716178d708a66e1a51efeca232
dguetlin <dirk.guetlin@gmail.com>
2021-12-18 15:33:53 +0100

    add asr init

---
+++ asr/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# (c) Dirk Gütlin, 2021. <dirk.guetlin@gmail.com>
+#
+# License: BSD-3-Clause
+
+"""
+Welcome to the ASRpy documentation.
+
+You find the documentation to all available functions by clicking on
+the respective submodules.
+
+"""
+
+from .asr import (ASR)
