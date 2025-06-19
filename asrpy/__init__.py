6321e5e9e9e0ad325cf35c472c12f47b26944aaa
Dirk Gütlin <34550289+DiGyt@users.noreply.github.com>
2025-06-19 16:51:35 +0200

    fixed docstring bumped version

--- asrpy/__init__.py
+++ asrpy/__init__.py
@@ -7,13 +7,14 @@ Welcome to the ASRpy documentation.
 
 ASR (Artefact Subspace Reconstruction) is a widely used automated cleaning
 algorithm for EEG data. This version of ASR is implemented to easily
-integrate with the MNE-Python toolbox for M/EEG analysis.
+integrate with the MNE-Python toolbox for M/EEG analysis. The original method
+was invented by Kothe & Jung (2016) for the EEGLab toolbox.
 
 You find the documentation to all available functions by clicking on
 the respective submodules.
 
 """
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 from .asr import ASR, asr_calibrate, asr_process, clean_windows  # noqa: F401
