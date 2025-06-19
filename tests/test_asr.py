bd51614c6d1060c8ddc46986c56d7ae5b0324913
DiGyt <dirk.guetlin@gmail.com>
2025-06-19 15:04:25 +0200

    made test pass flake

--- tests/test_asr.py
+++ tests/test_asr.py
@@ -32,30 +32,32 @@ def tes_asr():  # TODO: MNE DATA HAS CHANGED; SO THIS TEST DOESNT WORK ANYMORE
                             maxdims=0.66)
 
     # check if the data is highly equal to the MATLAB data
-    corrs = [np.corrcoef(i, j)[0, 1] for (i, j) in zip(cleaned.get_data(), valid_data)]
+    corrs = [np.corrcoef(i, j)[0, 1] for (i, j) in zip(cleaned.get_data(),
+                                                       valid_data)]
     assert np.mean(corrs) > 0.94
+
 
 def test_asr_calibrate():
     # Generate some random data
     X = np.random.randn(10, 1000)
-    
+
     # Run the calibration
     M, T = asr_calibrate(X, sfreq=100)
-    
+
     # Ensure the output shapes are correct
     assert M.shape == (10, 10)
     assert T.shape == (10, 10)
 
+
 def test_asr_process():
     # Generate some random data
     X = np.random.randn(10, 1000)
-    
+
     # Calibrate the ASR
     M, T = asr_calibrate(X, sfreq=100)
-    
+
     # Run the ASR processing
     cleaned = asr_process(X, sfreq=100, M=M, T=T)
-    
+
     # Ensure the output shape is correct
     assert cleaned.shape == X.shape
-
